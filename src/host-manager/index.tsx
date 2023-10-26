54427ff254905eb2f4d9bd3a483d3302d764984d
kilokid <arlomov007@gmail.com>
2023-10-27 01:55:00 +0300

    fix: hosts component structure and styles changed

--- src/host-manager/index.tsx
+++ src/host-manager/index.tsx
@@ -1,8 +1,22 @@
-import Module from "../shared/module";
+import { FC } from "react";
 
-const HostManager = () => {
+import Title from "../shared/title";
+import Popup from "../popup";
+import List from "../shared/list";
+
+import style from './index.module.css';
+
+const HostManager: FC = () => {
     return (
-        <Module moduleName="Hosts" />
+         <div>
+            <div className={style.moduleHeader}>
+                <Title moduleName='Hosts' />
+                <Popup>
+                    <h2>Add new host</h2>
+                </Popup>
+            </div>
+            <List />
+        </div>
     );
 };
 

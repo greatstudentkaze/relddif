bad656e90f59ecb64af0080b09617d9cf81da51f
kilokid <arlomov007@gmail.com>
2023-10-27 01:55:28 +0300

    fix: module-manager component structure & styles changed

--- src/module-manager/index.tsx
+++ src/module-manager/index.tsx
@@ -1,8 +1,22 @@
-import Module from '../shared/module';
+import { FC } from 'react';
 
-const ModuleManager = () => {
+import Popup from '../popup';
+import Title from '../shared/title';
+import List from '../shared/list';
+
+import style from './index.module.css';
+
+const ModuleManager: FC = () => {
     return (
-        <Module moduleName="Modules" />
+        <div>
+            <div className={style.moduleHeader}>
+                <Title moduleName='Modules' />
+                <Popup>
+                    <h2>Add new module</h2>
+                </Popup>
+            </div>
+            <List />
+        </div>
     );
 };
 
